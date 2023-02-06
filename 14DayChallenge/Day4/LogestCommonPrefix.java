102306793f91dd992bc6851edba61a7a4280f938
AswinSatheesh <aswinsatheesh2001@gmail.com>
2023-02-06 11:42:14 +0530

    Logest common prefix problem solved and learned

---
+++ 14DayChallenge/Day4/LogestCommonPrefix.java
@@ -0,0 +1,21 @@
+//Aswin Solution
+
+public class LogestCommonPrefix{
+    public String solution(String[] str){
+        if(str.length == 0) return "";
+        String prefix = str[0];
+        for(int i=1;i<str.length;i++){
+            while(str[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0,prefix.length()-1);
+                if(prefix.isEmpty()) return ""; 
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args){
+        LogestCommonPrefix obj = new LogestCommonPrefix();
+        String[] str = {"malaya","malayll","malayalam"};
+        System.out.println(obj.solution(str));
+    }
+}
