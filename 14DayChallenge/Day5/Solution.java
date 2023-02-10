52a1a774c284837b0e278ad4dd82137cbde5e3ce
AswinSatheesh <aswinsatheesh2001@gmail.com>
2023-02-10 13:36:39 +0530

    Aswins solution added valid parentheses

---
+++ 14DayChallenge/Day5/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i< s.length();i++)
+        {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()) return false;
+                char top  = st.peek();
+                if(top == '{' && ch == '}') st.pop();
+                else if(top == '(' && ch == ')') st.pop();
+                else if(top == '[' && ch == ']') st.pop();
+                else return false;
+            }
+        }
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s1 = "()";
+        String s2 = "()[]{}";
+        // String s3 = "(]";
+        // String s4 = "([)]";
+        // String s5 = "{[]}";
+
+        System.out.println(solution.isValid(s1));
+        System.out.println(solution.isValid(s2));
+        // System.out.println(solution.isValid(s3));
+        // System.out.println(solution.isValid(s4));
+        // System.out.println(solution.isValid(s5));
+    }
+}
---
+++ 14DayChallenge/Day5/ValidParenthesesSolution.java
@@ -0,0 +1,29 @@
+//Aswin solution 
+
+import java.util.Stack;
+class ValidParenthesesSolution{
+    public boolean isValidPara(String s){
+        Stack<Character> stk = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                stk.push(ch);
+            }
+            else{
+                if(stk.isEmpty()) return false;
+                char top = stk.peek();
+                if(top == '(' && ch == ')') stk.pop();
+                else if(top == '{' && ch == '}') stk.pop();
+                else if(top == '[' && ch == ']') stk.pop();
+                else return false;
+            }
+        }
+        return stk.isEmpty();
+    }
+    public static void main(String[] args){
+        ValidParenthesesSolution obj = new ValidParenthesesSolution();
+        String s1 = "()";
+        System.out.println(obj.isValidPara(s1));
+
+    }
+}
