9c276c8813396f811b268a11c8edea4a2c25649d
AswinSatheesh <aswinsatheesh2001@gmail.com>
2023-02-11 17:11:13 +0530

    reference code added need to learn how it works

---
+++ 14DayChallenge/Day6/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1, new ListNode(30, new ListNode(40)));
+        ListNode l2 = new ListNode(30, new ListNode(40, new ListNode(42)));
+
+        Solution solution = new Solution();
+        ListNode result = solution.mergeTwoLists(l1, l2);
+
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
