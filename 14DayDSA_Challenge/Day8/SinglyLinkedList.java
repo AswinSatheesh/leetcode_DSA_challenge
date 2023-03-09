d5253384ad69135351bed0ebb64f20c76dcdfc8c
AswinSatheesh <aswinsatheesh2001@gmail.com>
2023-03-09 07:30:48 +0530

    length of a linked list finded

--- 14DayDSA_Challenge/Day8/SinglyLinkedList.java
+++ 14DayDSA_Challenge/Day8/SinglyLinkedList.java
@@ -11,6 +11,7 @@ public class SinglyLinkedList{
             this.next = null;
         }
     }
+    //To display the Singly linked list
     public void display(){
         ListNode current = head;
         while(current != null){
@@ -19,15 +20,35 @@ public class SinglyLinkedList{
         }
         System.out.print(" null ");
     }
+
+    //To find the length of Singly linked list
+    public int length(){
+        int count = 0;
+        ListNode current = head;
+        while(current != null){
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
     public static void main(String[] args){
         SinglyLinkedList obj = new SinglyLinkedList();
         obj.head = new ListNode(10);
         ListNode second = new ListNode(20);
         ListNode third = new ListNode(30);
+        ListNode four = new ListNode(60);
+        ListNode five = new ListNode(67);
+
 
         obj.head.next = second;
         second.next = third;
+        third.next = four;
+        four.next = five;
 
         obj.display();
+        System.out.println();
+        System.out.println("Length of the singlyliked list is : " + obj.length());
+
+
     }
 }
