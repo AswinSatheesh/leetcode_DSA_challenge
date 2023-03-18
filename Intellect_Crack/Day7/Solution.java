fe08a7871152c1af98a6f4a68a70044871cd4a75
AswinSatheesh <aswinsatheesh2001@gmail.com>
2023-03-18 12:26:56 +0530

    Binary Tree traversal reference code added debugginh

---
+++ Intellect_Crack/Day7/Solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            result.add(curr.val);
+            curr = curr.right;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        Solution solution = new Solution();
+        List<Integer> result = solution.inorderTraversal(root);
+        System.out.println(result); // Output: [1, 3, 2]
+    }
+}
