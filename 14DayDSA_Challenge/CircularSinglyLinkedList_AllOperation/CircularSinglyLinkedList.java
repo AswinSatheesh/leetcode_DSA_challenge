3e8a5a7f4adbfd1c3f16060fbfb5514ebdc7a718
AswinSatheesh <aswinsatheesh2001@gmail.com>
2023-05-03 14:39:38 +0530

    display method added

--- 14DayDSA_Challenge/CircularSinglyLinkedList_AllOperation/CircularSinglyLinkedList.java
+++ 14DayDSA_Challenge/CircularSinglyLinkedList_AllOperation/CircularSinglyLinkedList.java
@@ -1,9 +1,9 @@
 public class CircularSinglyLinkedList{
-    Private ListNode last;
-    private int length ;
+    private ListNode last;
+    private int length;
 
     private class ListNode{
-        private ListNode next ;
+        private ListNode next;
         private int data;
 
         public ListNode(int data){
@@ -24,22 +24,38 @@ public class CircularSinglyLinkedList{
         return length == 0;
     }
 
+    //creating a new circular Linked list
     public void createCircularLinkedList(){
         ListNode first = new ListNode(1);
         ListNode second = new ListNode(5);
         ListNode third = new ListNode(10);
         ListNode fourth = new ListNode(15);
 
-        first.next = second;
+        first.next  = second;
         second.next = third;
-        third.next = fourth;
+        third.next  = fourth;
         fourth.next = first;
 
         last = fourth;
     }
 
+    //traverse and print (display)
+    public void display(){
+        if(last == null){
+            return;
+        }
+
+        ListNode first = last.next;
+        while(first != last){
+            System.out.print(first.data + " ");
+            first = first.next;
+        }
+        System.out.print(first.data);
+    }
+
     public static void main(String[] args){
         CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
         csll.createCircularLinkedList();
+        csll.display();
     }
 }
